5aad121dd414b33be45716dedb58e5e95a049427
Vertika Jain <vertikajain96@gmail.com>
2020-10-31 10:05:42 +0530

    PUT & DELETE Functionality #Day88

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express()
 
 const PORT = process.env.PORT || 5000
 
-const comics = [
+let comics = [
     { id: "1", name: "Archies" },
     { id: "2", name: "Champak" },
     { id: "3", name: "Batman" },
@@ -13,18 +13,17 @@ const comics = [
 ]
 
 app.use(express.json())
-// app.use(express.urlencoded({ extended: false }))
 
 // GET All Comics
 app.get("/comics", (req, res) => {
-    res.send(comics)
+    res.status(200).send(comics)
 })
 
 // GET Comic based on ID
 app.get("/comics/:id", (req, res) => {
     let comic = comics.find(c => c.id == parseInt(req.params.id))
-    if (!comic) res.status(404).send({ "error": "Comic not found." })
-    else res.send(comic)
+    if (!comic) return res.status(404).send({ "error": "Comic not found." })
+    res.status(200).send(comic)
 })
 
 // POST Comic
@@ -34,12 +33,42 @@ app.post("/comics", (req, res) => {
     })
     const result = schema.validate(req.body);
     if (result.error)
-        res.status(400).send({ error: result.error.details[0].message })
-    else {
-        let newComic = { id: comics.length + 1, name: req.body.name }
-        comics.push(newComic)
-        res.send(comics)
-    }
+        return res.status(400).send({ error: result.error.details[0].message })
+
+    let newComic = { id: comics.length + 1, name: req.body.name }
+    comics.push(newComic)
+    res.status(201).send(comics)
+})
+
+// PUT method to update Resources -> Comics
+app.put("/comics/:id", (req, res) => {
+    // Search
+    let comic = comics.find(c => c.id == parseInt(req.params.id))
+    if (!comic) return res.status(404).send({ "error": "Comic not found." })
+    // Validate
+    const schema = Joi.object({
+        name: Joi.string().min(3).required()
+    })
+    const result = schema.validate(req.body);
+    if (result.error)
+        return res.status(400).send({ error: result.error.details[0].message })
+    // Return
+    comics = comics.map(c => {
+        if (c.id == req.params.id)
+            c.name = req.body.name
+        return c
+    })
+    res.status(200).send(comics)
+})
+
+// DELETE comic
+app.delete("/comics/:id", (req, res) => {
+    // Search
+    let comic = comics.find(c => c.id == parseInt(req.params.id))
+    if (!comic) return res.status(404).send({ "error": "Comic not found." })
+    // Delete & Return
+    comics = comics.filter(c => c.id != comic.id)
+    res.send(comics)
 
 })
 
